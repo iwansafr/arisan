bbdccf7d90123075bea1a601558ebbaa7d379cb9
iwansafr <iwansafr@gmail.com>
2025-10-21 22:49:01 +0700

    resize modal size in transaction add

--- resources/views/livewire/transaction/transaction-list.blade.php
+++ resources/views/livewire/transaction/transaction-list.blade.php
@@ -1,10 +1,19 @@
 <?php
 
+use Carbon\Carbon;
 use Livewire\Volt\Component;
 use Livewire\Attributes\Computed;
 
 new class extends Component {
     
+    public $date;
+    public $member_id;
+
+    public function mount()
+    {
+        //next 2 weeks date
+        $this->date = Carbon::now()->addWeeks(2)->format('Y-m-d');
+    }
 
     #[Computed]
     public function members()
@@ -18,26 +27,28 @@ new class extends Component {
         <flux:button>Tambah Transaksi</flux:button>
     </flux:modal.trigger>
 
-    <flux:modal name="add-transaction">
-        <div class="space-y-6">
-            <div>
-                <flux:heading size="lg">Update profile</flux:heading>
-                <flux:text class="mt-2">Make changes to your personal details.</flux:text>
+    <flux:modal name="add-transaction" class="w-full">
+        <form wire:submit="save">
+            <div class="space-y-6">
+                <div>
+                    <flux:heading size="lg">Update profile</flux:heading>
+                    <flux:text class="mt-2">Make changes to your personal details.</flux:text>
+                </div>
+    
+                <flux:select wire:model="member_id" label="Member">
+                    @foreach ($this->members as $member)
+                        <flux:select.option value="{{ $member->id }}"> {{ $member->name }} </flux:select.option>
+                    @endforeach
+                </flux:select>
+    
+                <flux:input label="Tanggal" type="date" wire:model="date" />
+    
+                <div class="flex">
+                    <flux:spacer />
+    
+                    <flux:button type="submit" variant="primary">Save changes</flux:button>
+                </div>
             </div>
-
-            <flux:select>
-                @foreach ($this->members as $member)
-                    <flux:select.option value="{{ $member->id }}"> {{ $member->name }} </flux:select.option>
-                @endforeach
-            </flux:select>
-
-            <flux:input label="Date of birth" type="date" />
-
-            <div class="flex">
-                <flux:spacer />
-
-                <flux:button type="submit" variant="primary">Save changes</flux:button>
-            </div>
-        </div>
+        </form>
     </flux:modal>
 </div>
